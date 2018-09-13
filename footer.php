ec5685279a8467e9a19efb4ef32bd85f50fc58b8
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-13 11:06:08 -0700

    footer done but links need linking, waiting for page-renamning

--- footer.php
+++ footer.php
@@ -1,4 +1,20 @@
 		<?php wp_footer(); ?>
+		<footer>
+		<div class="container-fluid">
+			<div class="row h-100">
+				<div class="col-md-6 d-flex justify-content-center align-items-center h-100">
+					<p>Created by J.R. Ruggiero</p>
+				</div>
+				<div class="col-md-6 d-flex flex-column align-items-center justify-content-center">
+					<a href="#">Home</a>
+					<a href="#">Current Races</a>
+					<a href="#">Player Stats</a>
+					<a href="#">Game Stats</a>
+					<a href="#">Player Rankings</a>
+				</div>
+			</div>
+		</div>
+		</footer>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
