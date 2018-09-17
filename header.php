3f9a4fc53efb01fbd4c3f8c21c8646d3c0248fce
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-17 15:33:04 -0700

    nav bar build links dynamically according to pages

--- header.php
+++ header.php
@@ -14,23 +14,18 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarText">
 			<ul class="navbar-nav mr-auto">
-			<li class="nav-item<?php if(get_query_var('pagename') == '') echo (' active'); ?>">
-				<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item<?php if(get_query_var('pagename') == 'statpage') echo (' active'); ?>">
-				<a class="nav-link" href="/statpage">Player Stats</a>
-			</li>
-			<li class="nav-item<?php if(get_query_var('pagename') == 'ff4festats') echo (' active'); ?>">
-				<a class="nav-link" href="/ff4festats">Game Stats</a>
-			</li>
-			<li class="nav-item<?php if(get_query_var('pagename') == 'currentraces') echo (' active'); ?>">
-				<a class="nav-link" href="/currentraces">Current Races</a>
-			</li>
-			<li class="nav-item<?php if(get_query_var('pagename') == 'player-rankings') echo (' active'); ?>">
-				<a class="nav-link" href="/player-rankings">Player Rankings</a>
-			</li>
+				<li class="nav-item<?php if(get_query_var('pagename') == '') echo (' active'); ?>">
+					<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<?php $list_of_pages = get_pages(); 
+				// var_dump($list_of_pages); 
+				foreach ($list_of_pages as $page): ?>
+				<li class="nav-item<?php if(get_query_var('pagename') == $page->post_name) echo (' active'); ?>">
+					<a href="/<?php echo $page->post_name ?>" class="nav-link"><?php echo $page->post_title ?></a>
+				</li>
+				<?php endforeach; ?>
+			
 			</ul>
-			<?php echo get_query_var('pagename'); ?>
 			<?php $userinfo = wp_get_current_user(); 
 			if($userinfo->display_name == null) {
 				?><a href="<?php echo wp_login_url( get_permalink() ); ?>"><button class="btn btn-success">Login</button></a><?php
