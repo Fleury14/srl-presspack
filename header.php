d586e090eef8b0b8b83bea81db6aaf09dcb7db60
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-17 15:15:26 -0700

    navbar now assigns active class to proper element

--- header.php
+++ header.php
@@ -14,22 +14,23 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarText">
 			<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
+			<li class="nav-item<?php if(get_query_var('pagename') == '') echo (' active'); ?>">
 				<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item<?php if(get_query_var('pagename') == 'statpage') echo (' active'); ?>">
 				<a class="nav-link" href="/statpage">Player Stats</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item<?php if(get_query_var('pagename') == 'ff4festats') echo (' active'); ?>">
 				<a class="nav-link" href="/ff4festats">Game Stats</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item<?php if(get_query_var('pagename') == 'currentraces') echo (' active'); ?>">
 				<a class="nav-link" href="/currentraces">Current Races</a>
 			</li>
-			<li class="nav-item">
+			<li class="nav-item<?php if(get_query_var('pagename') == 'player-rankings') echo (' active'); ?>">
 				<a class="nav-link" href="/player-rankings">Player Rankings</a>
 			</li>
 			</ul>
+			<?php echo get_query_var('pagename'); ?>
 			<?php $userinfo = wp_get_current_user(); 
 			if($userinfo->display_name == null) {
 				?><a href="<?php echo wp_login_url( get_permalink() ); ?>"><button class="btn btn-success">Login</button></a><?php
