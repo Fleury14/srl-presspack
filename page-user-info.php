69cab7824b12760592094f5ecb23fb650af9bda3
J.R. Ruggiero <jrruggiero@shift3tech.com>
2018-09-18 11:08:41 -0700

    error handling on no user and rank displays

--- page-user-info.php
+++ page-user-info.php
@@ -1,7 +1,18 @@
 <?php get_header();
 
 $current_user = wp_get_current_user();
-
+// get user info based on display name
+$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'http://api.speedrunslive.com/stat?player=' . $current_user->display_name . '&game=ff4hacks&page=1',
+		CURLOPT_HTTPHEADER => array(
+			"Content-Type: application/json"
+		)  
+	));
+	$info = curl_exec($curl);
+	curl_close($curl);
+	$overall_stats = json_decode($info);
 ?>
 
 	<main role="main" aria-label="Content">
@@ -21,7 +32,13 @@ $current_user = wp_get_current_user();
 				<p>Email: <?php echo $current_user->user_email; ?></p>
 				<p>Display Name: <?php echo $current_user->display_name; ?></p>
 				
-
+				<?php if ($overall_stats->errorCode == 404): ?>
+				<p>There is no SRL data for your nickname.</p>
+				<?php endif; ?>
+				<?php if ($overall_stats->stats->rank !== null): ?>
+				<p>Rank: <?php echo $overall_stats->stats->rank; ?></p>
+				<?php endif; ?>
+				
 			</article>
 			<!-- /article -->
 
